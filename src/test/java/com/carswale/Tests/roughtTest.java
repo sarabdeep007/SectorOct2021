b3a52bfc1ff363a3e1c10438bbd9161975dae46b
sarabdeep <sarab_2001inn@yahoo.com>
2023-10-03 22:57:19 +0200

    First commit

---
+++ src/test/java/com/carswale/Tests/roughtTest.java
@@ -0,0 +1,12 @@
+package com.carswale.Tests;
+
+public class roughtTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("kiddan");
+	}
+
+}
