a5d9e4a1ae8096e61c1cd8eef8469f22cf2d79cf
sarabdeep <sarab_2001inn@yahoo.com>
2023-10-04 00:29:39 +0200

    test develop

---
+++ src/test/java/com/carswale/Tests/roughTest4.java
@@ -0,0 +1,13 @@
+package com.carswale.Tests;
+
+import org.testng.annotations.Test;
+
+public class roughTest4 {
+	
+	@Test
+	public void jhaali1()
+	{
+		System.out.println("kiuch v chakki jaayida");
+	}
+
+}
